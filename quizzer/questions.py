a8d0d55b97fe72d32a6370612924e583ccc69241
Nikhil CSB <csbnikhil@gmail.com>
2021-01-25 01:22:19 +0530

    add questions

---
+++ quizzer/questions.py
@@ -0,0 +1,17 @@
+QUESTIONS = [
+	{
+		'question': 'Which one is not an operator?',
+		'choices': ['**', '|', '++', '//'],
+		'answer_index': 2,
+	},
+	{
+		'question': 'What does REPL mean in Python?',
+		'choices': [
+			'Robustly Executed Python Languages',
+			'Read Execute Python Loop',
+			'Realtime Evaluated Python Language',
+			'Read Evaluate Print Loop',
+		],
+		'answer_index': 3,
+	},
+]
